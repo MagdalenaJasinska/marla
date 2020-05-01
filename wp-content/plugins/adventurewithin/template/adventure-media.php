b08bd72112b3c7e59caf80bb0538159cf8e3ddcd
Bartek <m.wronska1993@gmail.com>
2020-05-01 16:41:53 +0200

    update actors spotlight page, change the image at media page, update the list of interviews

--- wp-content/plugins/adventurewithin/template/adventure-media.php
+++ wp-content/plugins/adventurewithin/template/adventure-media.php
@@ -7,65 +7,32 @@
 	//swfobject.embedSWF(el.getAttribute('data'), el, 300, 120, 10);
 </script>
 <div class="main-box-content">
-	<div class="adv-col-12">
-		<div class="adv-col-2 adv-date-sec">4/24/2013</div>
-		<div class="adv-col-9 adv-media-right-sec">
-			<p><strong>"Awaking Matters with Cynthia Slon"/ BBS Radio <a class="adv-link" href="www.bbsradio.com">www.bbsradio.com</a></strong></p>
-			<p>Marla Phillips is interviewed by Cynthia Slon on all matter of energy healing and beyond.</p>
+    	<div class="adv-col-12">
+    		<div class="adv-col-2 adv-date-sec">08/15/2019</div>
+    		<div class="adv-col-9 adv-media-right-sec">
+    			<p><strong>"Transformational ENERGY HEALER"/ What The Fockery? <a class="adv-link" target="_blank" href="https://radiopublic.com/what-the-fockery-6Bp2lQ/s1!6daea"> www.radiopublic.com </a></strong></p>
+    			<p>Transformational Energy Healing is a modality of healing which corrects energy imbalances to assist in the natural healing of the physical body.
+                   Marla Phillips will help you do just that.</p>
+    			<audio src="<?php echo plugins_url('adventurewithin/assets/media/What_The_Fockery_2019-08-15.m4a');?>" controls></audio>
+    			</br>
+    		</div>
+    	</div>
 
-			<object id="myId" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="140">
-				<param name="movie" value="<?php echo plugins_url('adventurewithin/assets/media/dewplayer-vol.swf');?>" />
-        		<!--[if !IE]>-->
-				<object type="application/x-shockwave-flash" data="<?php echo plugins_url('adventurewithin/assets/media/dewplayer-vol.swf');?>" width="300" height="140">
-				<!--<![endif]-->
-				<div>
-					<!-- <h1>Alternative content</h1> -->
-					<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
-				</div>
-				<!--[if !IE]>-->
-				</object>
-				<!--<![endif]-->
-			</object>
-		</div>
-	</div>
 	<div class="adv-col-12">
-		<div class="adv-col-2 adv-date-sec">04-17-2008</div>
+		<div class="adv-col-2 adv-date-sec">04/24/2013</div>
 		<div class="adv-col-9 adv-media-right-sec">
-			<p><strong>"UFOs and the Paranormal"/ BBS Radio <a class="adv-link" href="www.bbsradio.com">www.bbsradio.com</a></strong></p>
-			<p>Marla Phillips and Patrice Cole are guests on Preston Dennett's hour-long show discussing energy healing and intuitive astrology.</p>
-			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="140">
-				<param name="movie" value="<?php echo plugins_url('adventurewithin/assets/media/UFOs_and_the_Paranormal_2008-04-17.swf');?>" />
-        		<!--[if !IE]>-->
-				<object type="application/x-shockwave-flash" data="<?php echo plugins_url('adventurewithin/assets/media/UFOs_and_the_Paranormal_2008-04-17.swf');?>" width="300" height="140">
-				<!--<![endif]-->
-				<div>
-					<!-- <h1>Alternative content</h1> -->
-					<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
-				</div>
-				<!--[if !IE]>-->
-				</object>
-				<!--<![endif]-->
-			</object>
+			<p><strong>"Awaking Matters with Cynthia Slon"/ BBS Radio <a class="adv-link" href="https://bbsradio.com/">www.bbsradio.com</a></strong></p>
+			<p>Marla Phillips is interviewed by Cynthia Slon on all matter of energy healing and beyond.</p>
+			</br>
 		</div>
 	</div>
 	<div class="adv-col-12">
 		<div class="adv-col-2 adv-date-sec">11/17/2007</div>
 		<div class="adv-col-9 adv-media-right-sec">
-			<p><strong>"UFOs and the Paranormal"/ BBS Radio <a class="adv-link" href="www.bbsradio.com">www.bbsradio.com</a></strong></p>
+			<p><strong>"UFOs and the Paranormal"/ BBS Radio <a class="adv-link" href="https://bbsradio.com/">www.bbsradio.com</a></strong></p>
 			<p>Marla Phillips discusses her experiences as an Energy Healing in a one-hour show hosted by Preston Dennett.</p>
-			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="140">
-				<param name="movie" value="<?php echo plugins_url('adventurewithin/assets/media/UFOs_and_the_Paranormal_2007-11-17.swf');?>" />
-        		<!--[if !IE]>-->
-				<object type="application/x-shockwave-flash" data="<?php echo plugins_url('adventurewithin/assets/media/UFOs_and_the_Paranormal_2007-11-17.swf');?>" width="300" height="140">
-				<!--<![endif]-->
-				<div>
-					<!-- <h1>Alternative content</h1> -->
-					<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
-				</div>
-				<!--[if !IE]>-->
-				</object>
-				<!--<![endif]-->
-			</object>
+		    <audio src="<?php echo plugins_url('adventurewithin/assets/media/UFOs_and_the_Paranormal_2007-11-17.m4a');?>" controls></audio>
+		    </br>
 		</div>
 	</div>
 </div>
